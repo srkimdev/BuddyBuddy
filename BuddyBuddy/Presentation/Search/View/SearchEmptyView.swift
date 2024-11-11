f5f5520fb28e9a31088be31e2bebd41bd0ba5657
Jisoo Ham <133845468+isakatty@users.noreply.github.com>
2024-11-11 19:58:24 +0900

    [Add] add search empty UI #6

---
+++ BuddyBuddy/Presentation/Search/View/SearchEmptyView.swift
@@ -0,0 +1,42 @@
+//
+//  SearchEmptyView.swift
+//  BuddyBuddy
+//
+//  Created by Jisoo Ham on 11/11/24.
+//
+
+import UIKit
+
+final class SearchEmptyView: BaseView {
+    private let emptyView: UIImageView = {
+        let view = UIImageView()
+        view.image = UIImage(named: "playground")
+        view.contentMode = .scaleAspectFill
+        return view
+    }()
+    
+    private let emptyText: UILabel = {
+        let view = UILabel()
+        view.text = "emptySearch".localized()
+        view.textColor = .gray1
+        view.font = .body
+        return view
+    }()
+    
+    override func setHierarchy() {
+        [emptyView, emptyText]
+            .forEach { addSubview($0) }
+    }
+    
+    override func setConstraints() {
+        emptyView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.width.height.equalTo(180)
+        }
+        emptyText.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(emptyView.snp.bottom).offset(10)
+            make.height.equalTo(15)
+        }
+    }
+}
--- BuddyBuddy/Presentation/Search/ViewController/SearchViewController.swift
+++ BuddyBuddy/Presentation/Search/ViewController/SearchViewController.swift
@@ -8,13 +8,59 @@
 import UIKit
 
 import RxSwift
+import SnapKit
 
 final class SearchViewController: BaseNavigationViewController {
     private let disposeBag: DisposeBag = DisposeBag()
     
     private let vm: SearchViewModel
     
+    private let searchController: UISearchController = {
+        let controller = UISearchController(searchResultsController: nil)
+        controller.searchBar.placeholder = "search".localized()
+        controller.searchBar.tintColor = .blue
+        controller.searchBar.sizeToFit()
+        controller.searchBar.setBackgroundImage(UIImage(), for: .any, barMetrics: .default)
+        let searchTextField = controller.searchBar.searchTextField
+        searchTextField.backgroundColor = .primary.withAlphaComponent(0.3)
+        searchTextField.clipsToBounds = true
+        searchTextField.layer.borderWidth = 1
+        searchTextField.layer.cornerRadius = 18
+        searchTextField.layer.borderColor = UIColor.primary.withAlphaComponent(0.3).cgColor
+        return controller
+    }()
+    private let searchEmptyView: SearchEmptyView = SearchEmptyView()
+    
     init(vm: SearchViewModel) {
         self.vm = vm
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let textField = searchController.searchBar.searchTextField
+        textField.subviews.first?.subviews.first?.removeFromSuperview()
+    }
+    
+    override func bind() {
+        
+    }
+    
+    override func setNavigation() {
+        super.setNavigation()
+        title = "Search"
+        
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+    }
+    
+    override func setHierarchy() {
+        [searchEmptyView]
+            .forEach { view.addSubview($0) }
+    }
+    
+    override func setConstraints() {
+        searchEmptyView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
     }
 }
