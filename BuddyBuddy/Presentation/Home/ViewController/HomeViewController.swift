848b06f9e5d02dbeeecd217c501d3ed25a4e8f8f
SunAra25 <tjsdkfk77@naver.com>
2024-11-12 13:29:49 +0900

    [Add] add properties in HomeViewController #5

--- BuddyBuddy/Presentation/Home/ViewController/HomeViewController.swift
+++ BuddyBuddy/Presentation/Home/ViewController/HomeViewController.swift
@@ -14,6 +14,58 @@ final class HomeViewController: BaseNavigationViewController {
     
     private let vm: HomeViewModel
     
+    private let scrollView: UIScrollView = {
+        let view = UIScrollView()
+        view.showsVerticalScrollIndicator = false
+        return view
+    }()
+    private let stackView: UIStackView = {
+        let view = UIStackView()
+        return view
+    }()
+    private let menuBtn: UIButton = {
+        let view = UIButton()
+        var config = UIButton.Configuration.plain()
+        
+        config.image = .menu
+        view.configuration = config
+        return view
+    }()
+    private let channelView: UIView = ChannelView()
+    private let memberAddBtn: UIButton = {
+        let view = UIButton()
+        var config = UIButton.Configuration.plain()
+        var attr = AttributedString.init("Add Member".localized())
+        
+        attr.font = UIFont.body
+        attr.foregroundColor = .gray1
+        
+        config.attributedTitle = attr
+        config.image = UIImage(systemName: "plus")
+        config.imagePlacement = .leading
+        config.imagePadding = 16
+        
+        view.configuration = config
+        return view
+    }()
+    private let emptyView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        return view
+    }()
+    private let floatingBtn: UIButton = {
+        let view = UIButton()
+        var config = UIButton.Configuration.plain()
+        
+        config.image = .newMessage
+        config.contentInsets = .init(top: 16.45, leading: 16.45, bottom: 16.45, trailing: 16.45)
+        config.baseBackgroundColor = .primary
+        
+        view.configuration = config
+        view.layer.cornerRadius = 25
+        return view
+    }()
+    
     init(vm: HomeViewModel) {
         self.vm = vm
     }
