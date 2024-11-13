c3c09124ec1e024353321b5a824cd8f63a1f4648
SunAra25 <tjsdkfk77@naver.com>
2024-11-13 14:56:06 +0900

    [Add] add channelTitleTableViewCell #5

---
+++ BuddyBuddy/Presentation/Home/View/ChannelTitleTableViewCell.swift
@@ -0,0 +1,40 @@
+//
+//  ChannelTitleTableViewCell.swift
+//  BuddyBuddy
+//
+//  Created by 아라 on 11/13/24.
+//
+
+import UIKit
+
+import SnapKit
+
+final class ChannelTitleTableViewCell: BaseTableViewCell {
+    private let titleLabel: UILabel = {
+        let view = UILabel()
+        return view
+    }()
+    private let chevronImgView: UIImageView = {
+        let view = UIImageView(image: UIImage(systemName: "chevron.down"))
+        return view
+    }()
+    
+    override func setHierarchy() {
+        [titleLabel, chevronImgView].forEach {
+            contentView.addSubview($0)
+        }
+    }
+    
+    override func setConstraints() {
+        titleLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalToSuperview().inset(13)
+        }
+        
+        chevronImgView.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.trailing.equalToSuperview().inset(13)
+            make.size.equalTo(24)
+        }
+    }
+}
