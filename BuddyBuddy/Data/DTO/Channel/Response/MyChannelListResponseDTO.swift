cdad76d85b0034975f65597cef3c1501eb766ae5
SunAra25 <tjsdkfk77@naver.com>
2024-11-19 15:41:29 +0900

    [Add] add Entity and DTO for Channel #24

---
+++ BuddyBuddy/Data/DTO/Channel/Response/MyChannelListResponseDTO.swift
@@ -0,0 +1,36 @@
+//
+//  MyChannelListResponseDTO.swift
+//  BuddyBuddy
+//
+//  Created by 아라 on 11/19/24.
+//
+
+import Foundation
+
+typealias MyChannelListResponseDTO = [MyChannelDTO]
+
+struct MyChannelDTO: Decodable {
+    let workspaceID, name: String
+    let description: String?
+    let coverImage, ownerID, createdAt: String
+
+    enum CodingKeys: String, CodingKey {
+        case workspaceID = "workspace_id"
+        case name, description, coverImage
+        case ownerID = "owner_id"
+        case createdAt
+    }
+}
+
+extension MyChannelDTO {
+    func toDomain() -> MyChannel {
+        return MyChannel(
+            workspaceID: workspaceID,
+            name: name,
+            description: description,
+            coverImage: coverImage,
+            ownerID: ownerID,
+            createdAt: createdAt
+        )
+    }
+}
---
+++ BuddyBuddy/Domain/Entity/Channel/MyChannelList.swift
@@ -0,0 +1,17 @@
+//
+//  MyChannelList.swift
+//  BuddyBuddy
+//
+//  Created by 아라 on 11/19/24.
+//
+
+import Foundation
+
+struct MyChannel {
+    let workspaceID: String
+    let name: String
+    let description: String?
+    let coverImage: String
+    let ownerID: String
+    let createdAt: String
+}
