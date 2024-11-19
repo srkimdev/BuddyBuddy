dec4b215d08f1222e6ab5500e0cf710c415fa986
SunAra25 <tjsdkfk77@naver.com>
2024-11-19 16:24:02 +0900

    [Add] add ChannelRepository #24

---
+++ BuddyBuddy/Data/Repository/Channel/DefaultChannelRepository.swift
@@ -0,0 +1,58 @@
+//
+//  DefaultChannelRepository.swift
+//  BuddyBuddy
+//
+//  Created by 아라 on 11/19/24.
+//
+
+import Foundation
+
+import RxSwift
+
+final class DefaultChannelRepository: ChannelRepositoryInterface {
+    private typealias Router = ChannelRouter
+    
+    private let service: NetworkProtocol
+    
+    init(service: NetworkProtocol) {
+        self.service = service
+    }
+    
+    func fetchMyChannelList(playgroundID: String) -> RxSwift.Single<Result<MyChannelList, any Error>> {
+        service.callRequest(
+            router: Router.fetchMyChannelList(playgroundID: playgroundID),
+            responseType: MyChannelListResponseDTO.self
+        )
+        .map { result in
+            switch result {
+            case .success(let dto):
+                return .success(dto.map { $0.toDomain() })
+            case .failure(let error):
+                return .failure(error)
+            }
+        }
+    }
+    
+    func fetchUnreadCountOfChannel(
+        playgroundID: String,
+        channelID: String,
+        after: Date?
+    ) -> RxSwift.Single<Result<UnreadCountOfChannel, any Error>> {
+        service.callRequest(
+            router: Router.countUnreads(
+                playgroundID: playgroundID,
+                channelID: channelID,
+                after: after
+            ),
+            responseType: UnreadCountOfChannelResponseDTO.self
+        )
+        .map { result in
+            switch result {
+            case .success(let dto):
+                return .success(dto.toDomain())
+            case .failure(let error):
+                return .failure(error)
+            }
+        }
+    }
+}
---
+++ BuddyBuddy/Domain/RepositoryInterface/Channel/ChannelRepositoryInterface.swift
@@ -0,0 +1,19 @@
+//
+//  ChannelRepositoryInterface.swift
+//  BuddyBuddy
+//
+//  Created by 아라 on 11/19/24.
+//
+
+import Foundation
+
+import RxSwift
+
+protocol ChannelRepositoryInterface {
+    func fetchMyChannelList(playgroundID: String) -> Single<Result<MyChannelList, Error>>
+    func fetchUnreadCountOfChannel(
+        playgroundID: String,
+        channelID: String,
+        after: Date?
+    ) -> Single<Result<UnreadCountOfChannel, Error>>
+}
