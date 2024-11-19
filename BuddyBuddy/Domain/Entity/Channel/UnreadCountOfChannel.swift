a2b3e708dfb1764dc18eefa3461bdcc90b081928
SunAra25 <tjsdkfk77@naver.com>
2024-11-19 15:59:44 +0900

    [Add] add Entity and DTO for unread count of channel #24

---
+++ BuddyBuddy/Domain/Entity/Channel/UnreadCountOfChannel.swift
@@ -0,0 +1,14 @@
+//
+//  UnreadCountOfChannel.swift
+//  BuddyBuddy
+//
+//  Created by 아라 on 11/19/24.
+//
+
+import Foundation
+
+struct UnreadCountOfChannel {
+    let channelID: String
+    let name: String
+    let count: Int
+}
