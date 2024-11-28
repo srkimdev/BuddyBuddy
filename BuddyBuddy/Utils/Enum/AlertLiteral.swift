faf41451bf224acb6934e52aeda71f0c0e26b894
Jisoo Ham <133845468+isakatty@users.noreply.github.com>
2024-11-29 00:40:55 +0900

    [Chore] modify localization about alert literal #35

--- BuddyBuddy/Utils/Enum/AlertLiteral.swift
+++ BuddyBuddy/Utils/Enum/AlertLiteral.swift
@@ -10,22 +10,27 @@ import Foundation
 enum AlertLiteral {
     case joinChannel(channelName: String)
     case exitChannel
-    case deleteChannel
-    case changeChannelAdmin
-    case selectChannelAdmin
+    case changeChannelAdmin(userName: String)
     
-    var toText: String {
+    var toTitle: String {
+        switch self {
+        case .joinChannel:
+            return "JoinChannel".localized()
+        case .exitChannel:
+            return "ExitChannel".localized()
+        case .changeChannelAdmin(let userName):
+            return "ChangeChannelAdminAlertTitle".localized(userName)
+        }
+    }
+    
+    var toMessage: String {
         switch self {
         case .joinChannel(let channelName):
             return "[\(channelName)] 채널에 참여하시겠습니까?"
         case .exitChannel:
             return ""
-        case .deleteChannel:
-            return ""
         case .changeChannelAdmin:
-            return ""
-        case .selectChannelAdmin:
-            return ""
+            return "ChangeChannelAdminAlertMessage".localized()
         }
     }
 }
