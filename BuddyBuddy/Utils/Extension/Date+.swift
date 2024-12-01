4813cb5c59e4a5fdc7589a3dd6c3c17ed954685d
SunAra25 <tjsdkfk77@naver.com>
2024-12-01 22:47:49 +0900

    [Chore] modify enum about date format #45

--- BuddyBuddy/Utils/Extension/Date+.swift
+++ BuddyBuddy/Utils/Extension/Date+.swift
@@ -10,7 +10,7 @@ import Foundation
 extension Date {
     func toString(format: BuddyDateFormatter) -> String {
         BuddyDateFormatter.standard.timeStyle = .none
-        BuddyDateFormatter.standard.dateFormat = format.rawValue
+        BuddyDateFormatter.standard.dateFormat = format.value
         return BuddyDateFormatter.standard.string(from: self)
     }
 }
